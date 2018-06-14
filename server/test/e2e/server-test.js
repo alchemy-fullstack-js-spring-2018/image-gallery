3d3530510af0be939fb8a137c57ac8740c8624f6
Jen Lipton <jkl.lipton@gmail.com>
2018-06-14 10:35:45 -0700

    server setup

---
+++ server/test/e2e/server-test.js
@@ -0,0 +1,10 @@
+const request = require('./request');
+const mongoose = require('mongoose');
+const assert = require('chai').assert;
+
+describe('Image Gallery API', () => {
+    
+    beforeEach(() => mongoose.connection.dropDatabase());
+
+
+});
