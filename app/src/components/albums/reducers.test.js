e9fb1a54ab4c5f2027fc2d711e7b47a3e77cca33
Ryan Manro <rmanro@gmail.com>
2018-06-14 10:03:55 -0700

    albums load and add reducers testing OK

--- app/src/components/albums/reducers.test.js
+++ app/src/components/albums/reducers.test.js
@@ -1,4 +1,4 @@
-import { albums } from './reducers';
+import { albums, ALBUMS_LOAD, ALBUM_ADD } from './reducers';
 
 describe.only('Album Reducers', () => {
 
@@ -7,5 +7,13 @@ describe.only('Album Reducers', () => {
     expect(state).toEqual([]);
   });
 
-  
+  it('Loads Albums', () => {
+    const state = albums(undefined, { type: ALBUMS_LOAD, payload: [{ title: 'album' }] });
+    expect(state).toEqual([{ title: 'album' }]);
+  });
+
+  it('Adds an Album', () => {
+    const state = albums([{ title: 'album1' }], { type: ALBUM_ADD, payload: { title: 'album2' } });
+    expect(state).toEqual([{ title: 'album1' }, { title: 'album2' }]);
+  });
 });
