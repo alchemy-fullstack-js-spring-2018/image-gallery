035a78b0b60e4eb5f56e8791ad32a2cb5b1c21b8
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-14 11:49:32 -0700

    add image placeholders

--- app/src/components/images/Images.js
+++ app/src/components/images/Images.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import styles from './Images.css';
 
 export default class Images extends Component {
 
   render() {
     return (
-      <div>
-        Images
+      <div className={styles.images}>
+        <div className="image-placeholder"></div>
+        <div className="image-placeholder"></div>
+        <div className="image-placeholder"></div>
+        <div className="image-placeholder"></div>
+        <div className="image-placeholder"></div>
       </div>
     );
   }
