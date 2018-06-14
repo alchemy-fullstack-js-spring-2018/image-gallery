eaecade402dae9fc93d9088066f72e25cae42884
Steele Walston <steele.walston@gmail.com>
2018-06-14 09:26:50 -0700

    album unit tests pass

---
+++ server/test/e2e/db.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const connect = require('../../lib/util/connect');
+const mongoose = require('mongoose');
+// const request = require('./request');
+
+before(() => connect(process.env.MONGODB_URI));    
+after(() => mongoose.connection.close());
+
+module.exports = {
+    dropCollection(name) {
+        return mongoose.connection.dropCollection(name)
+            .catch(err => {
+                if(err.codeName !== 'NamespaceNotFound') throw err;
+            });
+    },
+    // createToken(data = { }) {
+    //     return request
+    //         .post('/auth/signup')
+    //         .send(data)
+    //         .then(res => res.body.token);
+    // }
+};
