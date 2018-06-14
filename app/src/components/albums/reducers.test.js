9ca1eeac801182f12d73926298944d641916101b
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-14 09:43:45 -0700

    getAlbums reducer passes test

--- app/src/components/albums/reducers.test.js
+++ app/src/components/albums/reducers.test.js
@@ -1,19 +1,20 @@
 import {
   albums,
   ALBUMS_LOAD,
-  ALBUM_ADD } from './reducers';
+  ALBUM_ADD,
+  getAlbums } from './reducers';
+
+const album1 = {
+  title: 'kittens',
+  description: 'all the best kittens' 
+};
+
+const album2 = {
+  title: 'puppies',
+  description: 'only the cutest puppies'
+};
 
 describe('albums reducer', () => {
-
-  const album1 = {
-    title: 'kittens',
-    description: 'all the best kittens' 
-  };
-
-  const album2 = {
-    title: 'puppies',
-    description: 'only the cutest puppies'
-  };
 
   it('has a default value of an empty array', () => {
     const state = albums(undefined, {});
@@ -28,5 +29,13 @@ describe('albums reducer', () => {
   it('adds an album', () => {
     const state = albums([album1], { type: ALBUM_ADD, payload: album2 });
     expect(state).toEqual([album1, album2]);
+  });
+});
+
+describe('selectors', () => {
+  it('gets albums', () => {
+    const albums = [album1, album2];
+    const got = getAlbums({ albums });
+    expect(got).toEqual(albums);
   });
 });
