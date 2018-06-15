4c26c527ce75328e669936c2af719558f849e183
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-15 16:05:11 -0700

    adding an album via form works

--- app/src/components/albums/Albums.js
+++ app/src/components/albums/Albums.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getAlbums } from './reducers';
-import { loadAlbums } from './actions';
+import { loadAlbums, addAlbum } from './actions';
 
 class Albums extends Component {
   static propTypes = {
     albums: PropTypes.array,
-    loadAlbums: PropTypes.func.isRequired
+    loadAlbums: PropTypes.func.isRequired,
+    addAlbum: PropTypes.func.isRequired
+  };
+
+  state = {
+    title: '',
+    description: ''
   };
 
   componentDidMount() {
     this.props.loadAlbums();
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addAlbum(this.state);
+    this.setState({ title: '', description: '' });
+  };
+
   render() {
     const { albums } = this.props;
+    const { title, description } = this.state;
+
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <legend>Add an album:</legend>
+            <label>title<input type="text" value={title} onChange={({ target }) => this.setState({ title: target.value })}/></label>
+            <label>description<input type="text" value={description} onChange={({ target }) => this.setState({ description: target.value })}/></label>
+            <button type="submit">ADD</button>
+          </fieldset>
+        </form>
         <ul>
           {albums.map(album => <li key={album._id}>{album.title}</li>)}
         </ul>
@@ -30,5 +52,5 @@ export default connect(
   state => ({
     albums: getAlbums(state)
   }),
-  { loadAlbums }
+  { loadAlbums, addAlbum }
 )(Albums);
