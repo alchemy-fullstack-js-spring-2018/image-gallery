f5acb2cc48f6812db6655561f022ed677c72cb7d
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-15 15:49:39 -0700

    list of album names now render

--- app/src/components/albums/Albums.js
+++ app/src/components/albums/Albums.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getAlbums } from './reducers';
+import { loadAlbums } from './actions';
 
-export default class Albums extends Component {
+class Albums extends Component {
+  static propTypes = {
+    albums: PropTypes.array,
+    loadAlbums: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.loadAlbums();
+  }
 
   render() {
+    const { albums } = this.props;
     return (
       <div>
-        Albums
+        <ul>
+          {albums.map(album => <li key={album._id}>{album.title}</li>)}
+        </ul>
       </div>
     );
   }
-
 }
+
+export default connect(
+  state => ({
+    albums: getAlbums(state)
+  }),
+  { loadAlbums }
+)(Albums);
