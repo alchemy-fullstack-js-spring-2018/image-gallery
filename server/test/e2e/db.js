b575153507ba3a9c0fae15ff4853164803ecfb4b
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-15 15:40:16 -0700

    add test db

--- server/test/e2e/db.js
+++ server/test/e2e/db.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+// require('dotenv').config();
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
 // const request = require('./request');
 
-before(() => connect(process.env.MONGODB_URI));    
+before(() => connect('mongodb://localhost:27017/image-gallery-test'));    
 after(() => mongoose.connection.close());
 
 module.exports = {
