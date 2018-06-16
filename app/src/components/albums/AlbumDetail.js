bca2f6f97098bffdacd806e04a91c62528d5084e
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-16 10:40:29 -0700

    set up subroutes

--- app/src/components/albums/AlbumDetail.js
+++ app/src/components/albums/AlbumDetail.js
@@ -1,13 +1,15 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { loadAlbum } from './actions';
+import { loadAlbum, clearAlbum } from './actions';
 import { getCurrentAlbum } from './reducers';
 
 class AlbumDetail extends PureComponent {
 
   static propTypes = {
     loadAlbum: PropTypes.func.isRequired,
+    clearAlbum: PropTypes.func.isRequired,
     album: PropTypes.object,
     id: PropTypes.string
   };
@@ -16,21 +18,36 @@ class AlbumDetail extends PureComponent {
     this.props.loadAlbum(this.props.id);
   }
 
+  componentWillUnmount() {
+    this.props.clearAlbum();
+  }
+
   render() {
 
     const { album } = this.props;
 
     if(!album) return null;
 
-    const { title, description, images } = album;
+    const { title, description, images, _id } = album;
   
     return (
       <div>
         <h1>{title}</h1>
-        <h1>{description}</h1>
+        <h2>{description}</h2>
         <ul>
-          {images.map(image => <li key={image._id}><img src={image.url}/></li>)}
+          <li><Link to={`/albums/${_id}/images/thumbnail`}>thumbnail view</Link></li>
+          <li><Link to={`/albums/${_id}/images/gallery`}>gallery view</Link></li>
+          <li><Link to={`/albums/${_id}/images/list`}>list view</Link></li>
         </ul>
+        <Switch>
+          <Route path={`/albums/${_id}/images/thumbnail`} render={() => (
+            <ul>
+              {images.map(image => <li key={image._id}><img src={image.url}/></li>)}
+            </ul>)}/>
+          <Route path="/albums/:id/images/gallery" />
+          <Route path="/albums/:id/images/list" />
+          <Redirect to={`/albums/${_id}/images/thumbnail`} />
+        </Switch>
       </div>
     );
   }
@@ -40,5 +57,5 @@ export default connect(
   state => ({
     album: getCurrentAlbum(state)
   }),
-  { loadAlbum }
+  { loadAlbum, clearAlbum }
 )(AlbumDetail);
