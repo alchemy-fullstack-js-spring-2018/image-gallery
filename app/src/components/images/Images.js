d31b741040aac58fde0692c6c78e0114812fb48e
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-16 21:26:30 -0700

    get images component ready for images

--- app/src/components/images/Images.js
+++ app/src/components/images/Images.js
@@ -1,18 +1,23 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Thumbnails from '../albums/Thumbnails';
 import styles from './Images.css';
 
 export default class Images extends PureComponent {
+  
+  static propTypes = {
+    images: PropTypes.array
+  };
 
   render() {
+    const { images } = this.props;
+
+    if(!images) return null;
+
     return (
-      <div className={styles.images}>
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
+      <div className={styles.thumbnails}>
+        <Thumbnails/>
       </div>
     );
   }
-
 }
