df7111a0abcfd18eaa23339e7bfb01eced297cc5
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-16 20:51:11 -0700

    style home page

--- app/src/components/home/Home.js
+++ app/src/components/home/Home.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react';
+import styles from './Home.css';
 
 export default class Home extends PureComponent {
 
   render() {
     return (
-      <div>
-        Home
+      <div className={styles.home}>
+        <p>Save images from the internet, and organize them into albums.</p>
       </div>
     );
   }
