b38e8f64e10f6e6725222beddb69d64482192324
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-16 21:06:08 -0700

    add content to about page

--- app/src/components/about/About.js
+++ app/src/components/about/About.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
+import styles from './About.css';
 
 export default class About extends PureComponent {
 
   render() {
     return (
-      <div>
-        About
+      <div className={styles.about}>
+        <p>Steele and Keli made this using React and Redux.</p>
+        <p>Keli gave it a terrible name &ndash; it rhymes with &ldquo;abominable&rdquo; &ndash; to give them something to write about in the About page.</p>
       </div>
     );
   }
