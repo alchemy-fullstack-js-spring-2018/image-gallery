68966984b6890fa882c9a7f9725c888e9bdc7873
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-19 10:41:26 -0700

    setting up request subscribe and getStoredUser() function

--- app/src/store.js
+++ app/src/store.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { albums, images } from '../src/components/albums/reducers';
+import { user, checkedAuth } from '../src/components/auth/reducers';
 
 const rootReducer = combineReducers({
   albums,
-  images
+  images,
+  user,
+  checkedAuth
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
