8985c78d3484a9a149e67d686c228d122f606aa4
Steele Walston <steele.walston@gmail.com>
2018-06-19 09:43:05 -0700

    auth reducer tests pass

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -3,8 +3,18 @@ import { get, post } from './request';
 const URL = '/api';
 const ALBUMS_URL = `${URL}/albums`;
 const IMAGES_URL = `${URL}/images`;
+const AUTH_URL = `${URL}/auth`;
 
 export const getAllAlbums = () => get(ALBUMS_URL);
 export const postAlbum = album => post(ALBUMS_URL, album);
 export const getOneAlbum = id => get(`${ALBUMS_URL}/${id}`);
 export const postImage = image => post(IMAGES_URL, image);
+
+export const signin = credentials => post(`${AUTH_URL}/signin`, credentials);
+export const signup = credentials => post(`${AUTH_URL}/signup`, credentials);
+
+export const verifyUser = token => get(`${AUTH_URL}/verify`, { 
+  headers: {
+    Authorization: token
+  }
+});
--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './promise-middleware';
+import { user, checkedAuth } from '../components/auth/reducers';
 import { albums, album } from '../components/albums/reducers';
 
 const rootReducer = combineReducers({
   albums,
-  album
+  album,
+  user,
+  checkedAuth
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
