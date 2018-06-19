fe0c9e298f2b5a4ef4926766942ed3528eee216d
Jen Lipton <jkl.lipton@gmail.com>
2018-06-19 12:44:17 -0700

    front-end auth

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getCheckedAuth } from '../auth/reducers';
+import { tryLoadUser } from '../auth/actions';
 import hero from '../CssImages/flowers.jpg';
+import PrivateRoute from './PrivateRoute';
 import Home from '../Home';
 import Header from '../Nav';
 import Albums from '../Albums/Albums';
 import About from '../About';
+import PropTypes from 'prop-types';
 
-export default class App extends Component {
+class App extends Component {
+
+  static propTypes = {
+    tryLoadUser: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired
+  };
+
+  componentDidMount() {
+    this.props.tryLoadUser();
+  }
 
   render() {
+    const { checkedAuth } = this.props;
+
     return (
       <Router>
         <div>
           <Header/>
           <main>
+            { checkedAuth && 
             <Switch>
               <Route exact path="/" component={Home}/>
-              <Route path="/albums" component={Albums}/>
-              <Route path="/about" component={About}/>
+              <Route path="/auth" component={Auth}/>
+              <PrivateRoute path="/albums" component={Albums}/>
+              <PrivateRoute path="/about" component={About}/>
               <Redirect to="/"/>
             </Switch>
+            }
             <img src={hero} />
           </main>
         </div>
@@ -27,3 +46,8 @@ export default class App extends Component {
     );
   }
 }
+
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { tryLoadUser }
+)(App);
