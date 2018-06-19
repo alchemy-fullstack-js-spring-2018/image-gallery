6af4eb087248f8bb06c159e8122990d3c5a9a939
Steele Walston <steele.walston@gmail.com>
2018-06-19 09:25:49 -0700

    user and auth set up in back and

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
 const errorHandler = require('./util/error-handler');
 require('./models/register-plugins');
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+const auth = require('./routes/auth');
 const albums = require('./routes/albums');
 const images = require('./routes/images');
 
+app.use('/api/auth', auth);
 app.use('/api/albums', albums);
 app.use('/api/images', images);
 
