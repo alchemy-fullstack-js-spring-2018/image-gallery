ca8c2d257f67275947abb78c41cdf7f0e1015c04
Steele Walston <steele.walston@gmail.com>
2018-06-19 10:29:12 -0700

    log in and out route working with private route

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,29 +1,51 @@
 import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { tryLoadUser } from '../auth/actions';
+import { getCheckedAuth } from '../auth/reducers';
+import PrivateRoute from './PrivateRoute';
 import Home from '../home/Home';
 import About from '../about/About';
 import Images from '../images/Images';
 import Albums from '../albums/Albums';
 import Nav from '../nav/Nav';
+import Auth from '../auth/Auth';
 import styles from './App.css';
 import AlbumDetail from '../albums/AlbumDetail';
 
-export default class App extends PureComponent {
+class App extends PureComponent {
+
+  static propTypes = {
+    tryLoadUser: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired
+  };
+
+  componentDidMount() {
+    this.props.tryLoadUser();
+  }
+
   render() {
+
+    const { checkedAuth } = this.props;
+
     return (
       <Router>
         <div className={styles.app}>
           <h1>Albuminable</h1>
           <Nav/>
           <main>
+            { checkedAuth &&
             <Switch>
               <Route exact path="/" component={Home}/>
-              <Route exact path="/albums" component={Albums}/>
+              <Route path="/auth" component={Auth}/>
+              <PrivateRoute exact path="/albums" component={Albums}/>
               <Route path="/about" component={About}/>
-              <Route path="/images" component={Images}/>
-              <Route path="/albums/:id" render={({ match, location }) => <AlbumDetail location={location} id={match.params.id}/> }/>
+              <PrivateRoute path="/images" component={Images}/>
+              <PrivateRoute path="/albums/:id" render={({ match, location }) => <AlbumDetail location={location} id={match.params.id}/> }/>
               <Redirect to="/"/>
             </Switch>
+            }
           </main>
           <footer>I am the footer</footer>
         </div>
@@ -31,3 +53,8 @@ export default class App extends PureComponent {
     );
   }
 }
+
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { tryLoadUser }
+)(App);
--- app/src/components/nav/Nav.js
+++ app/src/components/nav/Nav.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getUser } from '../auth/reducers';
+import { logout } from '../auth/actions';
 import styles from './Nav.css';
 
-export default class Nav extends Component {
+class Nav extends Component {
+
+  static propTypes = {
+    user: PropTypes.object,
+    logout: PropTypes.func.isRequired
+  };
+
+  handleLogout = () => {
+    this.props.logout();
+  };
 
   render() {
+
+    const { user } = this.props;
+
     return (
       <nav className={styles.nav}>
         <ul>
@@ -12,9 +28,19 @@ export default class Nav extends Component {
           <li><NavLink to="/about" activeClassName="current">About</NavLink></li>
           <li><NavLink to="/albums" activeClassName="current">Albums</NavLink></li>
           <li><NavLink to="/images" activeClassName="current">Images</NavLink></li>
+          {
+            user
+              ? <li><NavLink to="/" onClick={this.handleLogout}>Logout</NavLink></li>
+              : <li><NavLink to="/auth" >Login</NavLink></li>
+          }
         </ul>
       </nav>
     );
   }
 
 }
+
+export default connect(
+  state => ({ user: getUser(state) }),
+  { logout }
+)(Nav);
