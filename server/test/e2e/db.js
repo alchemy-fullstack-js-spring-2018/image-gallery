41755f3561488639c7a638a4afb136d8121e6f05
Steele Walston <steele.walston@gmail.com>
2018-06-20 13:17:56 -0700

    small changes again

--- server/test/e2e/db.js
+++ server/test/e2e/db.js
@@ -1,7 +1,6 @@
-// require('dotenv').config();
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
-// const request = require('./request');
+
 
 before(() => connect('mongodb://localhost:27017/image-gallery-test'));    
 after(() => mongoose.connection.close());
@@ -12,11 +11,5 @@ module.exports = {
             .catch(err => {
                 if(err.codeName !== 'NamespaceNotFound') throw err;
             });
-    },
-    // createToken(data = { }) {
-    //     return request
-    //         .post('/auth/signup')
-    //         .send(data)
-    //         .then(res => res.body.token);
-    // }
+    }
 };
